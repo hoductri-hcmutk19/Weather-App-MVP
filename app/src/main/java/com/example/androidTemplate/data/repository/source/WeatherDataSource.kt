086de7fda7f4c0f0799c79e0a0811579e1a6e484
trihd-3422 <ho.duc.tri@sun-asterisk.com>
2024-03-14 14:57:56 +0700

    apply mvp structure

---
+++ app/src/main/java/com/example/androidTemplate/data/repository/source/WeatherDataSource.kt
@@ -0,0 +1,3 @@
+package com.example.androidTemplate.data.repository.source
+
+class WeatherDataSource
---
+++ app/src/main/java/com/example/androidTemplate/screen/presenter/WeatherInfoShowPresenter.kt
@@ -0,0 +1,3 @@
+package com.example.androidTemplate.screen.presenter
+
+class WeatherInfoShowPresenter
