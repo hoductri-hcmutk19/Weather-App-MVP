c37eda4274b2c10e0b4ccdbcacac29767dc6e318
hoductrihcmut123 <tri.ho_dt_bd@hcmut.edu.vn>
2024-03-21 15:33:14 +0700

    Initial API, fetch data

--- app/src/main/java/com/example/weather/data/repository/source/WeatherDataSource.kt
+++ app/src/main/java/com/example/weather/data/repository/source/WeatherDataSource.kt
@@ -1,3 +1,21 @@
 package com.example.weather.data.repository.source
 
-class WeatherDataSource
+import com.example.weather.data.model.City
+import com.example.weather.data.model.CurrentWeather
+import com.example.weather.screen.RequestCompleteListener
+
+interface WeatherDataSource {
+    /**
+     * Local
+     */
+    interface Local {
+        fun getCityLocal(listener: RequestCompleteListener<MutableList<City>>)
+    }
+
+    /**
+     * Remote
+     */
+    interface Remote {
+        fun getWeather(cityId: Int, listener: RequestCompleteListener<CurrentWeather>)
+    }
+}
--- app/src/main/java/com/example/weather/data/repository/source/local/WeatherLocalDataSource.kt
+++ app/src/main/java/com/example/weather/data/repository/source/local/WeatherLocalDataSource.kt
@@ -1,3 +1,27 @@
 package com.example.weather.data.repository.source.local
 
-class WeatherLocalDataSource
+import com.example.weather.data.model.City
+import com.example.weather.data.repository.source.WeatherDataSource
+import com.example.weather.screen.RequestCompleteListener
+import java.io.IOException
+
+class WeatherLocalDataSource : WeatherDataSource.Local {
+    override fun getCityLocal(listener: RequestCompleteListener<MutableList<City>>) {
+        try {
+            val cityList: MutableList<City> = mutableListOf()
+            // let presenter know the city list
+            listener.onRequestSuccess(cityList)
+        } catch (e: IOException) {
+            // let presenter know about failure
+            listener.onRequestFailed(e.localizedMessage!!)
+        }
+    }
+
+    companion object {
+        private var instance: WeatherLocalDataSource? = null
+
+        fun getInstance() = synchronized(this) {
+            instance ?: WeatherLocalDataSource().also { instance = it }
+        }
+    }
+}
