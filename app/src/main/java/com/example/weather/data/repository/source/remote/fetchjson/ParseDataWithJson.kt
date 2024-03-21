78116171bf6f011c7a0df9aaffff6b34f93d761a
hoductrihcmut123 <tri.ho_dt_bd@hcmut.edu.vn>
2024-03-21 15:33:14 +0700

    Initial API, fetch data

--- app/src/main/java/com/example/weather/data/repository/source/remote/fetchjson/ParseDataWithJson.kt
+++ app/src/main/java/com/example/weather/data/repository/source/remote/fetchjson/ParseDataWithJson.kt
@@ -1,3 +1,33 @@
 package com.example.weather.data.repository.source.remote.fetchjson
 
-class ParseDataWithJson
+import android.util.Log
+import com.example.weather.data.model.CurrentWeather
+import com.example.weather.utils.ext.notNull
+import org.json.JSONException
+import org.json.JSONObject
+
+class ParseDataWithJson {
+    fun parseJsonToData(jsonObject: JSONObject?): Any {
+        var data: Any = CurrentWeather()
+        try {
+            val item = parseJsonToObject(jsonObject)
+            item.notNull {
+                data = it
+            }
+        } catch (e: JSONException) {
+            Log.e("ParseDataWithJson", "parseJsonToData: ", e)
+        }
+        return data
+    }
+
+    private fun parseJsonToObject(jsonObject: JSONObject?): Any? {
+        try {
+            jsonObject?.notNull {
+                return ParseJson().weatherParseJson(it)
+            }
+        } catch (e: JSONException) {
+            Log.e("ParseDataWithJson", "parseJsonToData: ", e)
+        }
+        return null
+    }
+}
