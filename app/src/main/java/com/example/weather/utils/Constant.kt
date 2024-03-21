04b7826082426c1885fa6b15b6bb41da20b66d8a
hoductrihcmut123 <tri.ho_dt_bd@hcmut.edu.vn>
2024-03-21 14:48:07 +0700

    prepare resources

--- app/src/main/java/com/example/weather/utils/Constant.kt
+++ app/src/main/java/com/example/weather/utils/Constant.kt
@@ -1,3 +1,10 @@
 package com.example.weather.utils
 
-class Constant
+import com.example.weather.BuildConfig
+
+object Constant {
+    const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+    var BASE_API_KEY = "&appid=" + BuildConfig.APP_ID
+    const val KELVIN_TO_CELSIUS_NUMBER = 273.15
+    const val MPS_TO_KMPH_NUMBER = 3.6
+}
