6ea00ce24a81348b82969a30ae04f5e294bc58e8
hoductrihcmut123 <tri.ho_dt_bd@hcmut.edu.vn>
2024-03-26 13:18:51 +0700

    Save data to local database using SQLite

--- app/src/main/java/com/example/weather/data/repository/source/local/WeatherLocalDataSource.kt
+++ app/src/main/java/com/example/weather/data/repository/source/local/WeatherLocalDataSource.kt
@@ -1,11 +1,36 @@
 package com.example.weather.data.repository.source.local
 
 import android.content.Context
+import com.example.weather.data.model.Weather
 import com.example.weather.data.repository.source.WeatherDataSource
+import com.example.weather.data.repository.source.local.database.DBHelper
+import com.example.weather.data.repository.source.local.database.IDBHelper
 
 class WeatherLocalDataSource private constructor(
     private val context: Context?
 ) : WeatherDataSource.Local {
+
+    private val dbHelper: IDBHelper by lazy { DBHelper.getInstance(context) }
+
+    override fun insertWeather(current: Weather, hourly: Weather, daily: Weather) {
+        dbHelper.insertWeather(current, hourly, daily)
+    }
+
+    override fun getAllLocalWeathers(): List<Weather> {
+        return dbHelper.getAllData()
+    }
+
+    override fun getLocalWeather(id: String): Weather? {
+        return dbHelper.getWeather(id)
+    }
+
+    override fun getAllLocalOveralls(): List<Weather> {
+        return dbHelper.getAllOverall()
+    }
+
+    override fun deleteWeather(id: String) {
+        dbHelper.deleteWeather(id)
+    }
 
     companion object {
         private var instance: WeatherLocalDataSource? = null
